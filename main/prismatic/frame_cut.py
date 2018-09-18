e8dccf44c36c71b78d15fa2cd1c041bddb28f557
davic <davic@aawant.com>
2018-09-18 16:14:41 +0800

    add frame_cut

--- main/prismatic/frame_cut.py
+++ main/prismatic/frame_cut.py
@@ -1,3 +1,54 @@
 class FrameCut:
     def __init__(self):
-        pass
+        self.routes = [{'sbj': 'nsubj', 'pred': 'ROOT', 'obj': 'dobj'}]
+        self.conj = 'conj'
+
+    def frame_route_match(self, frame, route):
+        for route_key, route_value in route.items():
+            if route_value not in frame:
+                return False
+        return True
+
+    def get_master_frame(self, frames, route):
+        master_frames = []
+        for frame_id, frame in frames.items():
+            if self.frame_route_match(frame, route):
+                master_frames.append(frame)
+        return master_frames
+
+    def extract(self, frame, route):
+        result = route.copy()
+        for route_key, route_value in route.items():
+            result[route_key] = frame[route_value]
+        return result
+
+    def get_conj_frame_id(self, frame):
+        conj_frame_ids = []
+        if self.conj in frame:
+            conjs = frame[self.conj]
+            if isinstance(conjs, list):
+                conj_frame_ids.extend(conjs)
+            elif isinstance(conjs, str):
+                conj_frame_ids.append(conjs)
+        return conj_frame_ids
+
+    def match_frame_extract(self, master_frame, frames, route):
+        match_frames = [master_frame]
+        conj_frame_ids = self.get_conj_frame_id(master_frame)
+        for conj_frame_id in conj_frame_ids:
+            conj_frame = frames[conj_frame_id]
+            master_frame_copy = master_frame.copy()
+            del master_frame_copy[route['pred']]
+            del master_frame_copy[route['obj']]
+            master_frame_copy.update(conj_frame)
+            match_frames.append(master_frame_copy)
+        return match_frames
+
+    def run(self, frames):
+        results = []
+        for route in self.routes:
+            master_frames = self.get_master_frame(frames, route)
+            for master_frame in master_frames:
+                match_frames = self.match_frame_extract(master_frame, frames, route)
+                results.extend(match_frames)
+        return results
--- main/prismatic/frame_extraction.py
+++ main/prismatic/frame_extraction.py
@@ -1,42 +1,47 @@
 # -*- coding: utf-8 -*-
 import collections
 
+
 class NodeQueue:
     def __init__(self):
         self._list = collections.deque([])
-    def add(self,node):
+
+    def add(self, node):
         if not len(self._list):
             self._list.append(node)
         else:
             m = 0
             for i in range(len(self._list)):
                 if node.idx == self._list[i].idx:
-                    m=1
+                    m = 1
                     break
                 if node.node_depth >= self._list[i].node_depth:
-                    self._list.insert(0,node)
-                    m=1
+                    self._list.insert(0, node)
+                    m = 1
                     break
-            if m==0:
+            if m == 0:
                 self._list.append(node)
+
     def pop(self):
         return self._list.popleft()
+
     def __len__(self):
         return len(self._list)
+
 
 class FrameExtraction:
     
     def __init__(self):
         self.unused_relation = ['punct']
-    
-    def get_head_idx(self,arcs):
+
+    def get_head_idx(self, arcs):
         for arc in arcs:
             if arc.relation == 'ROOT':
                 idx = arc.idx
         return idx
 
-    def second_level_frame(self,arcs,node):  #最底层叶节点合并                                                                                   
-        frame = {'type':node.netag}
+    def second_level_frame(self, arcs, node):  # 最底层叶节点合并
+        frame = {'type': node.netag}
         for relation_type in node.children:
             if relation_type in self.unused_relation:
                 continue
@@ -46,19 +51,18 @@ class FrameExtraction:
                 frame[relation_type].append(arcs[child_idx].word)
                 frame[relation_type+'@node'] = str(arcs[child_idx].idx)
         return frame
-    
-    
-    def first_level_frame(self,arcs,node):  #SLR second_level_relation
-        frame = {'head_node':{'word':node.word,'node':node.idx,'SLR':{'type':node.netag}}}
+
+    def first_level_frame(self, arcs, node):  # SLR second_level_relation
+        frame = {'head_node': {'word': node.word, 'node': node.idx, 'SLR': {'type': node.netag}}}
         for relation_type in node.children:
             if relation_type in self.unused_relation:
                 continue
             for child_idx in node.children[relation_type]:
-                frame[relation_type] = {'word':arcs[child_idx].word,'node':arcs[child_idx].idx,'SLR':self.second_level_frame(arcs,arcs[child_idx])}
+                frame[relation_type] = {'word': arcs[child_idx].word, 'node': arcs[child_idx].idx,
+                                        'SLR': self.second_level_frame(arcs, arcs[child_idx])}
         return frame
-    
-    
-    def add_depth_arcs(self,arcs,idx,depth): #每个节点记录距根节点距离及到叶节点距离
+
+    def add_depth_arcs(self, arcs, idx, depth):  # 每个节点记录距根节点距离及到叶节点距离
         depth = depth+1
         node = arcs[idx]
         node.node_depth = depth
@@ -67,15 +71,14 @@ class FrameExtraction:
             return 0
         depths = []
         for child_index in node.children_indexes:
-            depths.append(self.add_depth_arcs(arcs,child_index,depth))
+            depths.append(self.add_depth_arcs(arcs, child_index, depth))
         node.tree_depth = max(depths) + 1
         return max(depths) + 1
-        
         
     def build_frames(self,arcs):
 
         root_idx = self.get_head_idx(arcs)
-        self.add_depth_arcs(arcs,root_idx,-1)
+        self.add_depth_arcs(arcs, root_idx, -1)
         
         depth_restriction = 2
         frames = {}
@@ -85,13 +88,13 @@ class FrameExtraction:
         
         for node in arcs:
             if node.tree_depth == depth_restriction and node.idx != root_idx:
-                frame = self.first_level_frame(arcs,node)
+                frame = self.first_level_frame(arcs, node)
                 frame_name = 'frame'+str(frame_idx)
                 frames[frame_name] = frame
                 node2frame_dict[node.idx] = frame_name
                 frame_idx += 1        
         
-        for frame_name,frame in frames.items():
+        for frame_name, frame in frames.items():
             node_idx = arcs[frame['head_node']['node']].head
             node_queue.add(arcs[node_idx])
         
@@ -101,19 +104,20 @@ class FrameExtraction:
         while len(node_queue):
             node = node_queue.pop()
             if node.relation == 'ROOT':
-                frame = {'ROOT':{'word':node.word,'node':node.idx,'SLR':{'type':node.postag}}}
+                frame = {'ROOT': {'word': node.word, 'node': node.idx, 'SLR': {'type': node.postag}}}
             else:
-                frame = {'head_node':{'word':node.word,'node':node.idx,'SLR':{'type':node.netag}}}
+                frame = {'head_node': {'word': node.word, 'node': node.idx, 'SLR': {'type': node.netag}}}
             
             for relation_type in node.children:
                 if relation_type in self.unused_relation:
                     continue
-                frame[relation_type] = []   #最末两级不是list 两级之上是list
+                frame[relation_type] = []   # 最末两级不是list 两级之上是list
                 for child_idx in node.children[relation_type]:
                     if child_idx in node2frame_dict:
                         frame[relation_type].append(node2frame_dict[child_idx])
                     else:
-                        frame[relation_type].append({'word':arcs[child_idx].word,'node':arcs[child_idx].idx,'SLR':self.second_level_frame(arcs,arcs[child_idx])})
+                        frame[relation_type].append({'word': arcs[child_idx].word, 'node': arcs[child_idx].idx,
+                                                     'SLR': self.second_level_frame(arcs, arcs[child_idx])})
             
             frame_name = 'frame' + str(frame_idx)
             frame_idx = frame_idx + 1
@@ -124,7 +128,3 @@ class FrameExtraction:
             else:
                 node_queue.add(arcs[node.head])
         return frames
-        
-        
-        
-        
